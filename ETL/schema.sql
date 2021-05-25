95c48ef14fe9e7ad0f2fe42adcd26afbfb1429b4
Damini-Patel <73375478+Damini-Patel@users.noreply.github.com>
2021-05-25 19:22:11 +0800

    completed ETL

---
+++ ETL/schema.sql
@@ -0,0 +1,11 @@
+--Create table for ecommerce data
+CREATE TABLE ecomau (
+id SERIAL PRIMARY KEY,
+date VARCHAR,
+year VARCHAR,
+month VARCHAR,
+online_food_turnover FLOAT,
+online_nonfood_turnover FLOAT,
+online_total_turnover FLOAT,
+total_revenue_turnover FLOAT
+);
