91e5af4690545f982777d8c657eb681899a90c66
Damini-Patel <73375478+Damini-Patel@users.noreply.github.com>
2021-05-29 11:06:52 +0800

    created 2 more tables for machine learning

--- ETL/schema.sql
+++ ETL/schema.sql
@@ -9,3 +9,25 @@ online_nonfood_turnover FLOAT,
 online_total_turnover FLOAT,
 total_revenue_turnover FLOAT
 );
+
+CREATE TABLE precovid (
+id SERIAL PRIMARY KEY,
+date VARCHAR,
+year VARCHAR,
+month VARCHAR,
+online_food_turnover FLOAT,
+online_nonfood_turnover FLOAT,
+online_total_turnover FLOAT,
+total_revenue_turnover FLOAT
+);
+
+CREATE TABLE postcovid (
+id SERIAL PRIMARY KEY,
+date VARCHAR,
+year VARCHAR,
+month VARCHAR,
+online_food_turnover FLOAT,
+online_nonfood_turnover FLOAT,
+online_total_turnover FLOAT,
+total_revenue_turnover FLOAT
+);
