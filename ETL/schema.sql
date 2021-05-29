9e508adbd4fb7cec36a9cac2a5fefe6659ad8c85
Damini-Patel <73375478+Damini-Patel@users.noreply.github.com>
2021-05-29 12:13:04 +0800

    ETL process completed

--- ETL/schema.sql
+++ ETL/schema.sql
@@ -31,3 +31,14 @@ online_nonfood_turnover FLOAT,
 online_total_turnover FLOAT,
 total_revenue_turnover FLOAT
 );
+
+CREATE TABLE ecommerce (
+id SERIAL PRIMARY KEY,
+date VARCHAR,
+year VARCHAR,
+month VARCHAR,
+online_food_turnover FLOAT,
+online_nonfood_turnover FLOAT,
+online_total_turnover FLOAT,
+total_revenue_turnover FLOAT
+);
